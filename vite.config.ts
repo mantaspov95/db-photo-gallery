fd0fccc04ce38484aacc45fad3d805561e6017a2
MantasPovilaitis <mantas.povilaitis@outlook.com>
2025-05-28 23:44:26 +0300

    1. removed placeholder code 2. initial folder structure 3. color pallette variable css 4. path aliases setup 5. placeholder layout div with comments for further actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src", //enable absolute imports from the src directory
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@context": "/src/context",
+      "@hooks": "/src/hooks",
+      "@pages": "/src/pages",
+      "@utils": "/src/utils",
+    },
+  },
+});

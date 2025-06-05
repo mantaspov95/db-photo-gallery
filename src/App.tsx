03e4b0248fe7ee59f3112cee370ea84018b1a662
mantaspov95 <mantas.povilaitis@teliacompany.com>
2025-06-05 21:39:41 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import type { ReactElement } from 'react';
+import styles from '@scss/app.module.scss';
 import Sidenav from './components/Sidenav/Sidenav';
 import ThemeProvider from './context/ThemeProvider';
-import styles from '@scss/app.module.scss';
+
 const App = (): ReactElement => (
   <ThemeProvider>
     <div className={`${styles['app-wrapper']}`}>

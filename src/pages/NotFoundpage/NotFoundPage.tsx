0d5c0c39540dd150c2493ea92eea974a9a57c4ca
mantaspov95 <mantas.povilaitis@teliacompany.com>
2025-07-01 08:06:45 +0300

    pr fixes - normalize.css, linting and arialabel to not found page logo

--- src/pages/NotFoundpage/NotFoundPage.tsx
+++ src/pages/NotFoundpage/NotFoundPage.tsx
@@ -6,9 +6,11 @@ import type { ReactElement } from 'react';
 const cx = classNames.bind(styles);
 
 const NotFoundPage = (): ReactElement => (
-  <div className={cx('not-found-page')}>
-    <Logo404 className={cx('not-found-page__logo')} />
-    <h1 className={cx('not-found-page__heading')}>Page Not Found</h1>
+  <div className={cx('not-found-page')} >
+    <Logo404 className={cx('not-found-page__logo')} aria-label="Error 404" />
+    <h1 id="not-found-title" className={cx('not-found-page__heading')}>
+      Page Not Found
+    </h1>
   </div>
 );
 export default NotFoundPage;
